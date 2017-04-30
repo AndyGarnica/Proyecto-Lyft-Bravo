773eb915507f5f429a0b596ad26fedb299b68521
AndyGarnica <elsa.garnica01@gmail.com>
2017-04-29 19:09:41 -0500

    Revert "Nancy"

--- js/main.js
+++ js/main.js
@@ -45,18 +45,17 @@ function prueba(){
 var numero = document.getElementById('numero');
 numero.addEventListener("click",aparecer);
 //variable para el placeholder
-// var fN = document.getElementsByClassName('fN');
+var fN = document.getElementsByClassName('fN');
 
-// for(var i=0; i< fN.length; i++){
-// 	fN[i].addEventListener('click',pequenoPlaceholder);
-// }
+for(var i=0; i< fN.length; i++){
+	fN[i].addEventListener('click',pequenoPlaceholder);
+}
 
-// //hacer una funcion para el fn
-// function pequenoPlaceholder(){
-// 	// this.style.fontSize = "5px";
-// 	this.placeholder = "hola";
+//hacer una funcion para el fn
+function pequenoPlaceholder(){
+	alert("hola");
 	
-// }
+}
 
 //funcion
 function aparecer(){
